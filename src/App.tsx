b25e927111eff7c8cb5893492a6497b4309d46e0
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-05-28 12:47:54 +0300

    useEffect and local storage added

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,96 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Table from "./components/Table";
 import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
 
-    let maxValue = 5;
-    let minValue = 0;
-    let [value, setValue] = useState(minValue);
+    let [maxValue, setMaxValueEdit] = useState(5)
+    const changeMaxValue = (newValue: number) => {
+        setMaxValueEdit(newValue)
+    }
 
+    let [minValue, setMinValueEdit] = useState(0)
+    const changeMinValue = (newValue: number) => {
+        setMinValueEdit(newValue)
+    }
+
+    const saveLocalStorage = () => {
+        localStorage.setItem('newMaxValue', JSON.stringify(maxValue))
+        localStorage.setItem('newMinValue', JSON.stringify(minValue))
+
+    }
+    useEffect(() => {
+        setMinValueEdit(minValue)
+    }, [minValue])
+    useEffect(() => {
+        setMaxValueEdit(maxValue)
+    }, [maxValue])
+    // получаем данные из localStorage
+    // const getLocalStorage = () => {
+    //   let newMaxValue = localStorage.getItem('newMaxValue')
+    //     let newMinValue = localStorage.getItem('newMinValue')
+    //     if (newMaxValue) {
+    //         setMaxValueEdit(JSON.parse(newMaxValue))
+    //     }
+    //     if (newMinValue) {
+    //         setMinValueEdit(JSON.parse(newMinValue))
+    //         setValue(JSON.parse(newMinValue))
+    //     }
+    //     console.log(maxValue, minValue)
+    //
+    // }
+
+    let [value, setValue] = useState(minValue);
     const incrementValue = () => {
         setValue(value + 1);
     }
-
     const resetValue = () => {
-        setValue(0);
+        setValue(minValue);
     }
 
+    useEffect(() => {
+        let newMaxValue = localStorage.getItem('newMaxValue')
+        let newMinValue = localStorage.getItem('newMinValue')
+        if (newMaxValue) {
+            setMaxValueEdit(JSON.parse(newMaxValue))
+        }
+        if (newMinValue) {
+            setMinValueEdit(JSON.parse(newMinValue))
+            setValue(JSON.parse(newMinValue))
+        }
+    }, [])
 
 
-  return (
-    <div className="App">
-        <div className={'wrapper'}>
-            <Table value={value} maxValue={maxValue} minValue={minValue}/>
-            <div className={'button-block'}>
-                <Button name={'INC'} callBack={incrementValue} disabled={maxValue} value={value}/>
-                <Button name={'RESET'} callBack={resetValue} disabled={minValue} value={value}/>
+
+    return (
+        <div className="App">
+            <div className={'wrapper'}>
+                <Table value={value} maxValue={maxValue} minValue={minValue}/>
+                <div className={'button-block'}>
+                    <Button name={'INC'} callBack={incrementValue} disabled={maxValue} value={value}/>
+                    <Button name={'RESET'} callBack={resetValue} disabled={minValue} value={value}/>
+                </div>
+            </div>
+            <div className={'wrapper'}>
+                <div className={'input-block'}>
+                    <Input value={maxValue} name={'Max value:'} callBack={changeMaxValue}/>
+                    <Input value={minValue} name={'Min value:'} callBack={changeMinValue}/>
+                </div>
+                <div className={'button-block'}>
+                    <Button name={'SET LocalStorage'}
+                            callBack={saveLocalStorage}
+                            disabled={maxValue}
+                            value={value}/>
+                    {/*<Button name={'GET LocalStorage'}*/}
+                    {/*        callBack={getLocalStorage}*/}
+                    {/*        disabled={10000}*/}
+                    {/*        value={value}/>*/}
+                </div>
             </div>
         </div>
-        {/*<div className={'wrapper'}>*/}
-        {/*    <Table />*/}
-        {/*    <div className={'button-block'}>*/}
-        {/*        <Button name={'SET'}/>*/}
-        {/*    </div>*/}
-        {/*</div>*/}
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type InputType = {
+    value: number
+    name: string
+    callBack: (newValue: number) => void
+}
+
+const Input = (props: InputType) => {
+    let [prevValue, setPrevValue] = useState(props.value)
+    const onChangeHandle = (e: ChangeEvent<HTMLInputElement>) => {
+        let newValue = e.currentTarget.value;
+        setPrevValue(JSON.parse(newValue));
+        props.callBack(JSON.parse(newValue));
+
+
+    }
+    return (
+        <div className={'inputBlock'}>
+            <span>{props.name}</span>
+            <input className={'input'} type='number' value={prevValue} onChange={onChangeHandle}/>
+        </div>
+    );
+};
+
+export default Input;
